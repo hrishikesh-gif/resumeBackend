59f5d0aa9637bf2ecd682beb54bc98b7c282ff0c
Hrishikesh Tayade <hrishikesht@Office.Arista>
2026-02-16 21:23:38 +0530

    database connected

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from .database import engine
-from .models import Base
+from .database import Base
+from . import models
 
 app = FastAPI()
 
 # Create tables automatically
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
---
+++ backend/app/models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    resumes = relationship("Resume", back_populates="owner")
+
+
+class Resume(Base):
+    __tablename__ = "resumes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    file_name = Column(String)
+    extracted_text = Column(Text)
+    match_score = Column(Integer)
+    is_good_fit = Column(Boolean)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="resumes")
